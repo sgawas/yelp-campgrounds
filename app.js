7fffaa1757f8f5805472723da63f8ae9e983e355
Suraj Gawas <suraj.gawas@nike.com>
2020-12-07 23:51:31 -0800

    heroku push2

--- app.js
+++ app.js
@@ -156,6 +156,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, ()=> {
-    console.log('listening on 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, ()=> {
+    console.log('listening on ', port);
 });
